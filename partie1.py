d2b0b671cd4c6c769841ac8906f40622832ed2ba
Marie Caron <marie.caron@epitech.eu>
2025-09-03 21:33:16 +0200

    feat: creation copy_characters function and test file.

--- partie1.py
+++ partie1.py
@@ -12,5 +12,20 @@ def write_text(filename, text) :
     filename.write(text)
     filename.close()
 
+def copy_characters(input_file, output_file, nb) :
+
+    nb = int(nb)
+
+    input_file = open(str(input_file), 'r')
+    copy_box = input_file.read(nb)
+    input_file.close()
+
+    output_file = open(str(output_file), 'a')
+    output_file.write('\n' * 2)
+    for character in copy_box :
+        output_file.write(character)
+    output_file.close()
+
 #read_on_line(input("Entrer le nom du fichier à ouvrir :"))
 #write_text(input("Entrer un nom de fichier à creer : "), input("Entrer le texte : "))
+#copy_characters(input("Entrer le fichier source : "), input("Entrer le fichier destination : "), input("Entrer le nombre de caractère à copier :"))
