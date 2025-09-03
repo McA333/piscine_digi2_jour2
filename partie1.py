c908bd238c5b802e96fc2e7a7321bef207a80be7
Marie Caron <marie.caron@epitech.eu>
2025-09-03 17:19:10 +0200

    feat: creation write_text function.

--- partie1.py
+++ partie1.py
@@ -4,4 +4,13 @@ def read_on_line(filename) :
     print(filename.readlines(1))
     filename.close()
 
+def write_text(filename, text) :
+
+    text = str(text)
+
+    filename = open(str(filename), 'w')
+    filename.write(text)
+    filename.close()
+
 #read_on_line(input("Entrer le nom du fichier à ouvrir :"))
+#write_text(input("Entrer un nom de fichier à creer : "), input("Entrer le texte : "))
