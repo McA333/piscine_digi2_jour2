3659d9e0472520b35af3215d42309a0a1343bdca
Marie Caron <marie.caron@epitech.eu>
2025-09-06 17:50:40 +0200

    feat: creation read_all_lines function. Read all lines and read every other line.

--- partie1.py
+++ partie1.py
@@ -26,6 +26,16 @@ def copy_characters(input_file, output_file, nb) :
         output_file.write(character)
     output_file.close()
 
+def read_all_lines(filename) :
+
+    filename = open(str(filename), 'r')
+    lines = (filename.readlines())
+    jump_line_by_two = lines[::2]
+
+    print(lines)
+    print(jump_line_by_two)
+
 #read_on_line(input("Entrer le nom du fichier à ouvrir :"))
 #write_text(input("Entrer un nom de fichier à creer : "), input("Entrer le texte : "))
 #copy_characters(input("Entrer le fichier source : "), input("Entrer le fichier destination : "), input("Entrer le nombre de caractère à copier :"))
+#read_all_lines(input("Entrer le nom du fichier : "))
