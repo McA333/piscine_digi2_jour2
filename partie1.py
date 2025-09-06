69529508a13ed4ecbd277449c1faa2f9a334fa1d
Marie Caron <marie.caron@epitech.eu>
2025-09-06 18:20:05 +0200

    feat: creation copy_characters_better function.

--- partie1.py
+++ partie1.py
@@ -18,12 +18,12 @@ def copy_characters(input_file, output_file, nb) :
 
     input_file = open(str(input_file), 'r')
     copy_box = input_file.read(nb)
-    input_file.close()
 
     output_file = open(str(output_file), 'a')
     output_file.write('\n' * 2)
     for character in copy_box :
         output_file.write(character)
+    input_file.close()
     output_file.close()
 
 def read_all_lines(filename) :
@@ -43,8 +43,21 @@ def write_text_better(filename, text) :
         filename.write(text)
         filename.close()
 
+def copy_characters_better(input_file, output_file, nb) :
+
+    nb = int(nb)
+
+    with open(input_file, 'r') as input_file, open(output_file, 'a') as output_file:
+        copy_box = input_file.read(nb)
+        output_file.write('\n' * 2)
+        for character in copy_box :
+            output_file.write(character)
+        input_file.close()
+        output_file.close()
+
 #read_on_line(input("Entrer le nom du fichier à ouvrir :"))
 #write_text(input("Entrer un nom de fichier à creer : "), input("Entrer le texte : "))
 #copy_characters(input("Entrer le fichier source : "), input("Entrer le fichier destination : "), input("Entrer le nombre de caractère à copier :"))
 #read_all_lines(input("Entrer le nom du fichier : "))
 #write_text_better(input("Entrer le nom du fichier : "),input("Entrer le text : "))
+#copy_characters_better(input("Entrer le fichier source : "), input("Entrer le fichier destination : "), input("Entrer le nombre de caractère à copier : "))
